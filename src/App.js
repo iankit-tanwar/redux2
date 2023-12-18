ed18b08d199d6eeb55711d589a7271c2e69b54a6
Ankit Tanwar <tankitofficial01@gmail.com>
2023-12-18 14:26:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { connect } from 'react-redux'
+
+
+function App(props) {
+
+  let handlechange= (e)=>{
+    props.b()
+  }
   return (
-    <div className="App">
+    <div className="App" >
+      {props.myStore}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,9 +25,23 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={(e)=>{handlechange(e)}}>clickme</button>
       </header>
     </div>
   );
 }
 
-export default App;
+let mapStateToProps = (state) => { // state == store object
+
+  return { myStore:state};
+}
+let mapDispatchToProps = (dispatch) => {
+ return {
+  a:function(){dispatch({type:'abc', payload:'ANKIT'})},
+  b:()=>{dispatch({type:'xyz',payload:"NAJAFGARH"})}
+ }
+
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {createStore} from 'redux'
+import { Provider } from 'react-redux';
+ let rootReducer = (initalState='Ankit',action)=>{
+  let newState = initalState; 
+
+  if(action.type==='abc'){
+   
+    return action.payload
+  }else if(action.type==='xyz'){
+    return action.payload
+  }else{
+    return newState;
+  }
+  
+  return newState;
+}
+
+let store = createStore(rootReducer);
+
+/* console.log(store)
+store.subscribe(()=>{
+  console.log(store.getState())
+})
+
+store.dispatch({type:'abcd', fname:'Tanwar'})  */
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store= {store}>
     <App />
+
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
